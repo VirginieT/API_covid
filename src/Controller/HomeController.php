6917791d9e11fc7ea754431114a7b0a8f9f3dd90
VirginieT <vtouzalin33@gmail.com>
2022-07-04 10:28:36 +0200

    update home template

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,7 @@ use App\Service\CallApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use http\Env\Request;
 
 class HomeController extends AbstractController
 {
