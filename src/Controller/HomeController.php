5115519aad66694ee4a1592f9ab228d7d1134396
VirginieT <vtouzalin33@gmail.com>
2022-07-22 17:56:45 +0200

    Update Homecontroller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,34 +15,13 @@ class HomeController extends AbstractController
      */
     public function index(CallApiService $callApiService): Response
     {
-        // dd($callApiService->getFranceData());
+         dd($callApiService->getAllData());
 
         return $this->render('home/index.html.twig', [
-            'data' => $callApiService->getFranceData(),
+            'data' => $callApiService->getAllData(),
+            'franceData' => $callApiService->getFranceData(),
         ]);
 
-
-
-
-        // for ($i = 1; $i < 8; $i++) {
-        //     $date = new DateTime('- '. $i .' day');
-        //     $datas = $callApiService->getAllDataByDate($date->format('d-m-Y'));
-
-        //     $label[] = $datas[0]['date'];
-        //     $hospitalisation[] = $datas[0]['incid_hosp'];
-        //     $rea[] = $datas[0]['incid_rea'];
-        // };
-      
-        // return $this->render('home/index.html.twig', [
-        //     'label' => $label,
-        //     'hospitalisation' => $hospitalisation,
-        //     'rea' => $rea,
-        // ]);
-        
-        // return $this->render('home/index.html.twig', [
-        //     'data' => $callApiService->getFranceData(),
-        //     'departements' => $callApiService->getAllData(),
-        // ]);
 
     }
 }
